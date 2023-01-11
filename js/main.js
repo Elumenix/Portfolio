3c3da4c23acefd31c9dd3fdba6a8517a6d0d0cc8
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-01-11 17:58:32 -0500

    Began setting up modals for the less intensive projects
    This was painful to figure out. Mainly took inspiration by inspecting other implementations of modals

---
+++ js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let body = document.querySelector("body");
+let skirmish = document.querySelector("#card-spaceway-skirmish");
+let modals = document.querySelectorAll(".modal");
+let projectCards = document.querySelectorAll(".project-card");
+let currentModal;
+
+
+// Send each card to its respective page
+skirmish.onclick = function() {window.location.href = "SpacewaySkirmish.html";};
+
+// set up project card click events
+projectCards.forEach(card => card.onclick = e => {
+    if (document.querySelector("#modal-" + card.id.substring(card.id.indexOf("-") + 1)) != null) {
+    let modal = document.querySelector("#modal-" + card.id.substring(card.id.indexOf("-") + 1));
+    modal.style.display = "block";
+    body.style.overflow = "hidden";
+    currentModal = modal;
+    }
+});
+
+// set up a way to close the modals
+for(let modal of modals) {
+      modal.querySelector(".modal-close").onclick = () => closeModal(modal);
+}
+window.onclick = e => {
+      for(let modal of modals) {
+            if(e.target == modal) {
+                  closeModal(modal);
+            }
+      }
+};
+
+// closes a modal
+function closeModal(modal) {
+      modal.style.display = "none";
+      body.style.overflow = "scroll";
+      currentModal = null;
+}
