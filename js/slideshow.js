34eb583089df40162d0e2aa6875a0ba0be7d579e
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-01-28 14:31:39 -0500

    Got slideshow working

--- js/slideshow.js
+++ js/slideshow.js
@@ -17,8 +17,8 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -26,13 +26,23 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 
   if (slideIndex - 1 != 0) {
-  slides[slideIndex - 1].style.width = "auto";
-  slides[slideIndex-1].querySelector("img").style.maxHeight = `${slides[0].querySelector("img").height}px`;
+    // Keeps aspect ratio of image
+    slides[slideIndex - 1].style.width = "auto";
+    //slides[slideIndex - 1].querySelector("img").style.margin.left = "auto";
+    //slides[slideIndex - 1].querySelector("img").style.margin.right = "auto";
+
+
+    // This makes it so that height of image 1 is relative to dom, not it's internal size
+    slides[0].style.display = "block";
+    slides[slideIndex - 1].querySelector("img").style.maxHeight = `${slides[0].querySelector("img").height}px`;
+    slides[slideIndex - 1].querySelector("img").style.display = "flex";
+    slides[slideIndex - 1].querySelector("img").style.margin = "0px auto"; // Center smaller images
+    slides[0].style.display = "none";
   }
 
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 } 
