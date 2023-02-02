c59cbf7289ff926b1dac4d6b2d3a3e43057c44e1
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-02-01 19:21:56 -0500

    Made progress with css and the crossboa page

--- js/main.js
+++ js/main.js
@@ -2,31 +2,35 @@
 
 let body = document.querySelector("body");
 let skirmish = document.querySelector("#card-spaceway-skirmish");
+let crossboa = document.querySelector("#card-crossboa");
 let modals = document.querySelectorAll(".modal");
 let projectCards = document.querySelectorAll(".project-card");
 let currentModal;
 
 
 // Send each card to its respective page
-skirmish.onclick = function() {window.location.href = "SpacewaySkirmish.html";};
+window.onload = () => {
+skirmish.onclick = () => { window.location.href = "SpacewaySkirmish.html";};
+crossboa.onclick = () => { window.location.href = "Crossboa.html";};
+}
 
 // set up project card click events
 projectCards.forEach(card => card.onclick = e => {
-    if (document.querySelector("#modal-" + card.id.substring(card.id.indexOf("-") + 1)) != null) {
-    let modal = document.querySelector("#modal-" + card.id.substring(card.id.indexOf("-") + 1));
-    modal.style.display = "block";
-    body.style.overflow = "hidden";
-    currentModal = modal;
-    }
+      if (document.querySelector("#modal-" + card.id.substring(card.id.indexOf("-") + 1)) != null) {
+            let modal = document.querySelector("#modal-" + card.id.substring(card.id.indexOf("-") + 1));
+            modal.style.display = "block";
+            body.style.overflow = "hidden";
+            currentModal = modal;
+      }
 });
 
 // set up a way to close the modals
-for(let modal of modals) {
+for (let modal of modals) {
       modal.querySelector(".modal-close").onclick = () => closeModal(modal);
 }
 window.onclick = e => {
-      for(let modal of modals) {
-            if(e.target == modal) {
+      for (let modal of modals) {
+            if (e.target == modal) {
                   closeModal(modal);
             }
       }
