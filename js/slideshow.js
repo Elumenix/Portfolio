491d2f342ff9d1d15ff5bf3a52ac2854817a6478
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-02-27 11:23:44 -0500

    Most of the logic for slideshows is fixed

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,42 +1,35 @@
 class Slideshow {
-  constructor(slideIndex, slideClassName, dotClassName, captionId) {
+  constructor(slideIndex, slideClassName, dotClassName, captionId, btnClass) {
     this.slideIndex = slideIndex;
     this.slideClassName = slideClassName;
     this.dotClassName = dotClassName;
     this.captionId = captionId;
     this.doit;
-
     this.showSlides(this.slideIndex);
-
-
-    document.querySelectorAll(".prev").onclick = () => this.plusSlides(-1);
-    document.querySelectorAll(".next").onclick = () => this.plusSlides(1);
-
+    document.querySelectorAll(`.prev` + btnClass).forEach((btn) => {
+      btn.onclick = () => this.plusSlides(-1);
+    });
+    document.querySelectorAll(`.next` + btnClass).forEach((btn) => {
+      btn.onclick = () => this.plusSlides(1);
+    });
     let items = document.querySelectorAll(this.dotClassName);
     let i;
-
-    // First several elements
     for (i = 0; i < items.length - 1; i++) {
       items[i].onclick = () => this.currentSlide(i + 1);
     }
-
-    // Final Element 
     items[i].onclick = () => this.currentSlide(0);
-
     window.onresize = () => {
       clearTimeout(this.doit);
       this.doit = setTimeout(this.resizedw, 100);
     };
   }
 
-  // Next/previous controls
   plusSlides = (n) => {
-    this.showSlides(slideIndex += n);
+    this.showSlides(this.slideIndex += n);
   }
 
-  // Thumbnail image controls
   currentSlide = (n) => {
-    this.showSlides(slideIndex = n);
+    this.showSlides(this.slideIndex = n);
   }
 
   showSlides = (n) => {
@@ -44,7 +37,7 @@ class Slideshow {
     let slides = document.querySelectorAll(this.slideClassName);
     let dots = document.querySelectorAll(this.dotClassName);
     let captionText = document.querySelector(this.captionId);
-    if (n > slides.length) { slideIndex = 1 }
+    if (n > slides.length) { this.slideIndex = 1 }
     if (n < 1) { this.slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
@@ -52,37 +45,26 @@ class Slideshow {
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
-
     slides[this.slideIndex - 1].style.display = "block";
     dots[this.slideIndex - 1].className += " active";
-
     if (this.slideIndex - 1 != 0) {
       // Keeps aspect ratio of image
       slides[this.slideIndex - 1].style.width = "auto";
-      //slides[slideIndex - 1].querySelector("img").style.margin.left = "auto";
-      //slides[slideIndex - 1].querySelector("img").style.margin.right = "auto";
-
-
-      // This makes it so that height of image 1 is relative to dom, not it's internal size
       slides[0].style.display = "block";
       slides[this.slideIndex - 1].querySelector("img").style.maxHeight = `${slides[0].querySelector("img").height}px`;
       slides[this.slideIndex - 1].querySelector("img").style.display = "flex";
       slides[this.slideIndex - 1].querySelector("img").style.margin = "0px auto"; // Center smaller images
       slides[0].style.display = "none";
     }
-
     captionText.innerHTML = dots[this.slideIndex - 1].alt;
   }
 
-
-  // Resizes images on a timer
   resizedw = () => {
-    // Haven't resized in 100ms!
     this.showSlides(this.slideIndex);
   }
 }
 
 window.onload = () => {
-  const slideshow1 = new Slideshow(1, ".mySlides1", ".demo1", "#caption1");
-  const slideshow2 = new Slideshow(1, ".mySlides2", ".demo2", "#caption2");
+  const slideshow1 = new Slideshow(1, ".mySlides1", ".demo1", "#caption1", "#slides1");
+  const slideshow2 = new Slideshow(1, ".mySlides2", ".demo2", "#caption2", "#slides2");
 }
